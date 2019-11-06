7c6b81394a5e5dcc8990375af99f4557c048c8b1
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2019-11-06 22:08:03 +0100

    add a test, cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 use rocket::http::Status;
 use rocket::request::{self, FromRequest, Request};
-use rocket::Outcome;
-use rocket::State;
+use rocket::{Outcome, Rocket, State};
+
+#[macro_use]
+extern crate dotenv_codegen;
 
 #[macro_use]
 extern crate rocket;
 
 struct ApiKey(String);
 
-struct MyConfig {
+struct JWKS {
     user_val: String,
 }
 
-/// Returns true if `key` is a valid API key string.
 fn is_valid(key: &str) -> bool {
-    key == "valid_api_key"
+    key == "bearer "
 }
 
 #[derive(Debug)]
@@ -29,10 +30,10 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
     type Error = ApiKeyError;
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let u = request.guard::<State<MyConfig>>().unwrap();
+        let u = request.guard::<State<JWKS>>().unwrap();
 
         println!("{:?}", u.user_val);
-        let keys: Vec<_> = request.headers().get("x-api-key").collect();
+        let keys: Vec<_> = request.headers().get("Authentication").collect();
         match keys.len() {
             0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
             1 if is_valid(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
@@ -43,16 +44,32 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
 }
 
 #[get("/")]
-fn index(key: ApiKey, state: State<MyConfig>) -> &'static str {
+fn index(_key: ApiKey) -> &'static str {
     "Hello, world!\n"
 }
 
-fn main() {
-    let config = MyConfig {
-        user_val: "user input".to_string(),
+fn rocket() -> Rocket {
+    let config = JWKS {
+        user_val: dotenv!["AUTHSERVER"].to_string(),
     };
-    rocket::ignite()
-        .mount("/", routes![index])
-        .manage(config)
-        .launch();
+    rocket::ignite().mount("/", routes![index]).manage(config)
+}
+
+fn main() {
+    rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::{Header, Status};
+    use rocket::local::Client;
+
+    #[test]
+    fn index() {
+        let header = Header::new("Authentication", "bearer ");
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.get("/").header(header).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
 }

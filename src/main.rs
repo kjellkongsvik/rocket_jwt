8058d425e07196df72b6b831a69240305f00b326
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2019-11-17 21:50:31 +0100

    WIP: HS256

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+use jsonwebtoken as jwt;
 use rocket::http::Status;
 use rocket::request::{self, FromRequest, Request};
-use rocket::{get, routes, Outcome, Rocket, State};
+use rocket::{get, routes, Outcome, Rocket};
+use serde_derive::{Deserialize, Serialize};
 
 struct ApiKey(String);
 
-struct JWKS {
-    user_val: String,
+fn is_valid(key: &str) -> bool {
+    let mut b = key.split_whitespace();
+    match b.next() {
+        Some(s) if s == "bearer" => (),
+        _ => return false,
+    };
+    let token = match b.next() {
+        Some(t) => {
+            println!("{}", t);
+
+            match jwt::decode::<Claims>(t, "secret".as_ref(), &jwt::Validation::default()) {
+                Ok(c) => Some(c),
+                Err(e) => {
+                    panic!("{}", e);
+                }
+            }
+        }
+        _ => None,
+    };
+    match token {
+        Some(_) => true,
+        _ => false,
+    }
 }
 
-fn is_valid(key: &str) -> bool {
-    key == "bearer "
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    company: String,
+    exp: usize,
 }
 
 #[derive(Debug)]
@@ -24,9 +49,6 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
     type Error = ApiKeyError;
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let u = request.guard::<State<JWKS>>().unwrap();
-
-        println!("{:?}", u.user_val);
         let keys: Vec<_> = request.headers().get("Authentication").collect();
         match keys.len() {
             0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
@@ -43,10 +65,7 @@ fn index(_key: ApiKey) -> &'static str {
 }
 
 fn rocket() -> Rocket {
-    let config = JWKS {
-        user_val: "AUTHSERVER".to_string(),
-    };
-    rocket::ignite().mount("/", routes![index]).manage(config)
+    rocket::ignite().mount("/", routes![index])
 }
 
 fn main() {
@@ -55,13 +74,21 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use super::rocket;
+    use super::{rocket, Claims};
+    use jsonwebtoken as jwt;
     use rocket::http::{Header, Status};
     use rocket::local::Client;
 
     #[test]
-    fn index() {
-        let header = Header::new("Authentication", "bearer ");
+    fn hello() {
+        let claims = Claims {
+            company: "ACME".to_owned(),
+            exp: 10000000000,
+        };
+
+        let token = jwt::encode(&jwt::Header::default(), &claims, "secret".as_ref()).unwrap();
+
+        let header = Header::new("Authentication", "bearer ".to_owned() + &token);
         let client = Client::new(rocket()).expect("valid rocket instance");
         let response = client.get("/").header(header).dispatch();
         assert_eq!(response.status(), Status::Ok);

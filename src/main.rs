20ade7501a2fd5f2c9bb93a3e5b12d61e638c01e
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2019-11-28 22:19:01 +0100

    bearer

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,8 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 use jsonwebtoken as jwt;
-use rocket::http::Status;
-use rocket::request::{self, FromRequest, Request};
-use rocket::{get, routes, Outcome, Rocket};
+use rocket::{get, routes, Rocket};
+
 use serde_derive::{Deserialize, Serialize};
-
-struct ApiKey(String);
-
-fn is_valid(key: &str) -> bool {
-    let mut b = key.split_whitespace();
-    match b.next() {
-        Some(s) if s == "bearer" => (),
-        _ => return false,
-    };
-    let token = match b.next() {
-        Some(t) => {
-            println!("{}", t);
-
-            match jwt::decode::<Claims>(t, "secret".as_ref(), &jwt::Validation::default()) {
-                Ok(c) => Some(c),
-                Err(e) => {
-                    panic!("{}", e);
-                }
-            }
-        }
-        _ => None,
-    };
-    match token {
-        Some(_) => true,
-        _ => false,
-    }
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -38,34 +10,33 @@ struct Claims {
     exp: usize,
 }
 
-#[derive(Debug)]
-enum ApiKeyError {
-    BadCount,
-    Missing,
-    Invalid,
+#[get("/token")]
+fn token() -> String {
+    let claims = Claims {
+        company: "ACME".to_string(),
+        exp: 10000000000,
+    };
+
+    let token = jwt::encode(&jwt::Header::default(), &claims, "secret".as_ref()).unwrap();
+
+    "Bearer ".to_string() + &token
 }
 
-impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
-    type Error = ApiKeyError;
-
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let keys: Vec<_> = request.headers().get("Authentication").collect();
-        match keys.len() {
-            0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
-            1 if is_valid(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
-            1 => Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid)),
-            _ => Outcome::Failure((Status::BadRequest, ApiKeyError::BadCount)),
-        }
+fn parse_bearer(bearer_token: &str) -> Result<String, String> {
+    let mut tk = bearer_token.split_whitespace();
+    match tk.next() {
+        Some(b) if b == "Bearer" => (),
+        _ => return Err("Not a bearer".to_string()),
     }
-}
-
-#[get("/")]
-fn index(_key: ApiKey) -> &'static str {
-    "Hello, world!\n"
+    let token = tk.next().ok_or("missing token")?;
+    if tk.next().is_some() {
+        return Err("more than enough".to_string());
+    }
+    Ok(token.to_string())
 }
 
 fn rocket() -> Rocket {
-    rocket::ignite().mount("/", routes![index])
+    rocket::ignite().mount("/", routes![token])
 }
 
 fn main() {
@@ -74,23 +45,42 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use super::{rocket, Claims};
-    use jsonwebtoken as jwt;
-    use rocket::http::{Header, Status};
+    use super::parse_bearer;
+    use super::rocket;
+    use rocket::http::Status;
     use rocket::local::Client;
 
     #[test]
-    fn hello() {
-        let claims = Claims {
-            company: "ACME".to_owned(),
-            exp: 10000000000,
-        };
+    fn missing_bearer() {
+        assert_eq!(parse_bearer(""), Err("Not a bearer".to_string()));
+    }
 
-        let token = jwt::encode(&jwt::Header::default(), &claims, "secret".as_ref()).unwrap();
+    #[test]
+    fn missing_token() {
+        assert_eq!(parse_bearer("Bearer "), Err("missing token".to_string()));
+    }
 
-        let header = Header::new("Authentication", "bearer ".to_owned() + &token);
+    #[test]
+    fn more_than_a_token() {
+        assert_eq!(
+            parse_bearer("Bearer a b"),
+            Err("more than enough".to_string())
+        );
+    }
+
+    #[test]
+    fn parse_token() {
+        assert_eq!(parse_bearer("Bearer a"), Ok("a".to_string()));
+    }
+
+    #[test]
+    fn token() -> Result<(), String> {
         let client = Client::new(rocket()).expect("valid rocket instance");
-        let response = client.get("/").header(header).dispatch();
+        let mut response = client.get("/token").dispatch();
         assert_eq!(response.status(), Status::Ok);
+
+        let bearer_token = response.body_string().ok_or("error")?;
+        let _token = parse_bearer(&bearer_token)?;
+        Ok(())
     }
 }
